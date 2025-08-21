6feca1d441e3be76fa0eff769e9ab30091f07510
Wil-cristian <127247211+Wil-cristian@users.noreply.github.com>
2025-08-20 22:34:20 -0700

    Create service_request.dart

---
+++ shared/models/service_request.dart
@@ -0,0 +1,13 @@
+class ServiceRequest {
+  final String id;
+  final String serviceName;
+  final String description;
+  final String status;
+
+  ServiceRequest({
+    required this.id,
+    required this.serviceName,
+    required this.description,
+    required this.status,
+  });
+}
