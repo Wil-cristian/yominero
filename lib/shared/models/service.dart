000c10c40bd29b68359e450292bccb4a26d0ebf8
Wil-cristian <127247211+Wil-cristian@users.noreply.github.com>
2025-08-23 21:27:08 -0700

    Add Service model class

---
+++ lib/shared/models/service.dart
@@ -0,0 +1,11 @@
+class Service {
+  final String id;
+  final String name;
+  final String description;
+
+  Service({
+    required this.id,
+    required this.name,
+    required this.description,
+  });
+}
